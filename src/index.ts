42d886707a9e316cb6da52326ee1da14000df7fa
Jacob Bednarz <jacob.bednarz@gmail.com>
2022-08-29 09:15:03 +1000

    methods tidy up

--- src/index.ts
+++ src/index.ts
@@ -5,32 +5,30 @@ const router = Router();
 
 router.get("/ping", () => new Response("PONG"));
 
-router.get("/status/:status", async (request) => {
+router.get("/status/:status", async (r) => {
   let statusText: string = "OK";
   let statusCode = 200;
 
-  if (
-    typeof request?.params?.status !== "undefined" &&
-    request?.params?.status !== null
-  ) {
-    statusCode = parseInt(request.params.status);
+  if (typeof r?.params?.status !== "undefined" && r?.params?.status !== null) {
+    statusCode = parseInt(r.params.status);
     statusText = statusCodeToText?.get(statusCode) || "OK";
   }
 
   return new Response(null, {
     status: statusCode,
     statusText: statusText,
+    headers: { "content-type": "text/plain" },
   });
 });
 
-router.all("/delay/:timeout", async (request: Request) => {
-  let timeout = parseInt(request?.params?.timeout) || 3;
-  await new Promise((r) => setTimeout(r, 1000 * timeout));
+router.all("/delay/:timeout", async ({ params }) => {
+  const timeout = parseInt(params.timeout) || 3;
+  await new Promise((r) => setTimeout(r, 1000 * timeout, {}));
   return new Response(null);
 });
 
-router.get("/content-type/:content_type", (request: Request) => {
-  switch (request?.params?.content_type) {
+router.get("/content-type/:content_type", (r: Request) => {
+  switch (r?.headers.get("content_type")) {
     case "json": {
       return new Response(JSON.stringify({ foo: "bar", baz: "qux" }), {
         headers: { "content-type": "application/json" },
@@ -44,6 +42,11 @@ router.get("/content-type/:content_type", (request: Request) => {
         }
       );
     }
+    case "plain": {
+      return new Response(`PONG`, {
+        headers: { "content-type": "text/plain" },
+      });
+    }
     default: {
       return new Response(
         `<html><head><title>foo</title></head><body>bar</body></html>`,
@@ -55,22 +58,44 @@ router.get("/content-type/:content_type", (request: Request) => {
   }
 });
 
-router.get("/ip", async (request: Request) => {
-  return new Response(request.headers.get("cf-connecting-ip"));
+router.get("/request", async (r: Request) => {
+  const data = {
+    request: {
+      event: r,
+      cf: r.cf,
+      headers: Object.fromEntries(r.headers),
+    },
+  };
+
+  return new Response(JSON.stringify(data, null, 2), {
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+    },
+  });
 });
 
-router.get("/headers", async (request: Request) => {
-  return new Response(JSON.stringify(Object.fromEntries(request.headers)), {
+router.get("/ip", async (r: Request) => {
+  return new Response(r.headers.get("cf-connecting-ip"), {
+    headers: { "content-type": "text/plain" },
+  });
+});
+
+router.get("/headers", async (r: Request) => {
+  return new Response(JSON.stringify(Object.fromEntries(r.headers)), {
     headers: { "content-type": "application/json" },
   });
 });
 
-router.get("/user-agent", async (request: Request) => {
-  return new Response(request.headers.get("user-agent"));
+router.get("/user-agent", async (r: Request) => {
+  return new Response(r.headers.get("user-agent"), {
+    headers: { "content-type": "text/plain" },
+  });
 });
 
 router.get("/uuid", () => {
-  return new Response(crypto.randomUUID());
+  return new Response(crypto.randomUUID(), {
+    headers: { "content-type": "text/plain" },
+  });
 });
 
 router.all(
@@ -78,13 +103,13 @@ router.all(
   () =>
     new Response(
       JSON.stringify({
-        "/all": "Returns everything about the request and response.",
         "/content-type/:content_type":
           "Returns a response with the designated content type and example content.",
         "/delay/:timeout": "Delays a response for the designated `timeout`.",
         "/headers": "Display all HTTP headers.",
         "/ip": "Get the callers IP.",
         "/ping": "Returns a PONG.",
+        "/request": "Returns everything about the request and response.",
         "/status/:status":
           "Returns a response and status code matching the requested status code.",
         "/user-agent": "Return the user agent.",
@@ -94,6 +119,4 @@ router.all(
     )
 );
 
-addEventListener("fetch", (event) =>
-  event.respondWith(router.handle(event.request))
-);
+addEventListener("fetch", (e) => e.respondWith(router.handle(e.request)));
